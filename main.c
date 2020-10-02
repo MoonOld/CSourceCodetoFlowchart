613030105ba30846d1759cca8a6d2a71077ab34c
MoonOld <431002206@qq.com>
2020-10-02 17:27:39 +0800

    built a simple lexer frame and go for dinner

--- main.c
+++ main.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+int token;            // current token
+char *src, *old_src;  // pointer to source code string;
+int poolsize;         // default size of text/data/stack
+int line;             // line number
+
+
+
+//tokens and classes defined here
+enum{
+    Num = 128, Fun, Sys, Glo, Loc, Id,
+    Char, Else, Enum, If, Int, Return, Sizeof, While,
+    Assign, Cond, Lor, Lan, Or, Xor,And, Eq, Ne, Lt,Gt, Le, Ge, Shl, Shr,
+    Add,Sub,Mul,Div, Mod, Inc, Dec,
+    Brk
+};
+
+
+void next(){
+    char *last_pos;
+    int hash;
+
+    //while(token = *src++){
+
+    //}
+    return;
+}
+
 
 int main() {
     printf("Hello, World!\n");
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
+==My internship in bingyan again. Hope for "GL&HF"==
+
+
+
 ## 十月二日
 
 * 复习Git，创建开发环境并将其push到github上
-
-  
+* 去图书馆借来龙书和两本实现教程用于学习阅读
+* 在github上找到c4（c in 4 functions）项目，及一个模仿c4写编译器的博客教程，预计将作为主要参考资料。
+* 根据博客写好词法分析函数next（）的框架
+* 复习c语言词法准备开始莽
